3f7df1660965f2c5961207aafca09c2c2a6f6ecc
Paulo Oliveira <paulo@riachao.com>
2023-08-22 17:41:38 -0300

    Sessão 5 - Listagem, filtro  e load do banco

--- Front/Fit-App/src/app/alunos/alunos.component.ts
+++ Front/Fit-App/src/app/alunos/alunos.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-alunos',
@@ -8,23 +10,49 @@ import { Component } from '@angular/core';
 })
 
 export class AlunosComponent {
-  public alunos: any ;
+  public alunos: any = [] ;
+  public alunosFiltrados: any = [];
+  exibirImagem: boolean = false;
+  private _filtroLista:string = "";
 
-  constructor(private http: HttpClient){
+
+  constructor(private http: HttpClient,private datePipe: DatePipe){
 
   }
 
+
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.alunosFiltrados = this.filtroLista ? this.filtrarAlunos(this.filtroLista) : this.alunos;
+  }
+
+  filtrarAlunos(filtrarPor: string): any{
+      filtrarPor = filtrarPor.toLocaleLowerCase();
+      return this.alunos.filter((aluno: any) => aluno.nomeAluno.toLocaleLowerCase().indexOf(filtrarPor) !== -1 );
+  }
 ngOnInit(): void {
   this.getAlunos();
+
+ // console.log('Alunos: ', this.alunos);
+}
+
+exibirOcultarImagem(){
+  this.exibirImagem = !this.exibirImagem;
 }
 
   public getAlunos(): void {
     this.http.get('https://localhost:7183/api/Fit').subscribe(
-      response => this.alunos = response,
+      response => {this.alunos = response; this.alunosFiltrados = response;},
       error => console.log(error)
     );
+  }
 
-    this.alunos = [{Nome: "Paulo"}];
+  public formatarData(dData:any){
+    return this.datePipe.transform(dData,"dd/MM/yyyy");
   }
 
 }
--- Front/Fit-App/src/app/app.module.ts
+++ Front/Fit-App/src/app/app.module.ts
@@ -1,25 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlunosComponent } from './alunos/alunos.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { CommonModule, DatePipe } from '@angular/common';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlunosComponent
+    AlunosComponent,
+    NavComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    
+    CollapseModule.forRoot(),
+    FormsModule,
+    CommonModule
+
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
